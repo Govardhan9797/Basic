ba7a83d55523918ecfe82f1687d2ed2784711ef8
Govardhan reddy Danduvari <85842506+Govardhan9797@users.noreply.github.com>
2024-08-16 19:03:17 +0530

    Create Rough.py

---
+++ Rough.py
@@ -0,0 +1,8 @@
+## basic programing in the python based on my understanding on every topic
+print('Hello, World') #prints the basic line Hello, World as output
+a = int(25)
+b=int(20)
+c = print(a + b) #Addition
+d=print(a-b)#sub
+e=print(a*b)#multiply
+f=print(a/b)#divide
